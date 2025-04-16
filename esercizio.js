ed1c1b3500e1a3126ea33031b849ce778ce9b3d0
Francescodib05 <dibenedettofrancesco971@gmail.com>
2025-04-16 10:37:08 +0200

    Esercizio2

--- esercizio.js
+++ esercizio.js
@@ -1,38 +1,50 @@
 function getPastEvent() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const events = [
-          { year: 1990, name: "World Wide Web Invented" },
-          { year: 2001, name: "Wikipedia Launched" },
-          { year: 1969, name: "Moon Landing" },
-          { year: 2020, name: "Global Pandemic" },
-          { year: 1989, name: "Fall of Berlin Wall" }
-        ];
-        const randomIndex = Math.floor(Math.random() * events.length);
-        resolve(events[randomIndex]);
-      }, Math.random() * 1000);
-    });
-  }
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const events = [
+        { year: 1990, name: "World Wide Web Invented" },
+        { year: 2001, name: "Wikipedia Launched" },
+        { year: 1969, name: "Moon Landing" },
+        { year: 2020, name: "Global Pandemic" },
+        { year: 1989, name: "Fall of Berlin Wall" }
+      ];
+      //console.log(events.length);
+      
+      const randomIndex = Math.floor(Math.random() * events.length);
+      resolve(events[randomIndex]);
+    }, Math.random() * 100);
+  });
+}
 
-let arr = [];
 
 
-async function travelThroughHistory (n=10) {
+async function travelThroughHistory(n=10) {
   try {
-    const nr=[]
+    const nr = []
     for (let i = 0; i < n; i++) {
       nr.push(getPastEvent())
-     
+
     }
     const result = await Promise.all(nr)
+   
+    if (arr < 95) {
+      for (let i = 0; i < n; i++) {
+        nr.push(getPastEvent())
+  
+      }
+      const result = await Promise.all(nr)
+    }
     
+
     ///console.log(result);
     
-    arr=result.sort((a,b) => a.year - b.year).filter((result,i,arr) => i === arr.findIndex(event => event.name === result.name)).filter(res => res.year< 2000);
+    arr = result.sort((a, b) => a.year - b.year).filter((result, i, arr) => i === arr.findIndex(event => event.name === result.name)).filter(res => res.year < 2000);
+    
     console.log(arr);
 
     return arr;
-    
+
+   
 
   } catch (error) {
     console.log(error.message);
